af83b9350df67eb9d4cb33d2afaee8f6acb9056b
Benzaoui <benzaoui.soufiane@dataimpact.fr>
2020-04-19 12:01:16 +0200

    Setup PWA

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,29 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'TV Series and Movies Quotes',
+  plugins: [{
+      use: 'gridsome-plugin-service-worker',
+      options: {
+        networkFirst: {
+          routes: [
+            "/",
+            /\.(js|css|png)$/, // means "every JS, CSS, and PNG images"
+          ],
+        },
+
+      }
+    },
+    {
+      use: "gridsome-plugin-manifest",
+      options: {
+        background_color: "#000000",
+        icon_path: "./src/assets/logo.png",
+        name: "TV Series and Movies Quotes",
+        short_name: "App",
+        theme_color: "#FFFFFF",
+        lang: "en",
+      }
+    }
+  ]
 }
