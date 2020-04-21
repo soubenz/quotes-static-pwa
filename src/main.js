4d3216de3ec9840b84b7f0bae797bcaf06f39ca1
Benzaoui <benzaoui.soufiane@dataimpact.fr>
2020-04-21 08:40:32 +0200

    Install and setup vuetify globally

--- src/main.js
+++ src/main.js
@@ -2,8 +2,31 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+export default function (Vue, {
+  appOptions,
+  head
+}) {
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css',
+  })
 
-export default function (Vue, { router, head, isClient }) {
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Material+Icons',
+  })
+
+
+  const opts = {
+    icons: {
+      iconfont: 'mdi',
+    },
+  }
+  Vue.use(Vuetify)
+  appOptions.vuetify = new Vuetify(opts);
+
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
